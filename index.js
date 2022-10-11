209fa3e9430ca011c4aaee6f16ef2796de41022e
hup-en <hupen.paija@gmail.com>
2022-10-11 22:31:42 +0545

    json file updated

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 var blog = [
-    {id: 1, name: "Hupen Paija", imgurl: "", url:""},
-    {id: 2, name: "Ram Pun", imgurl: 55, url:""},
-    {id: 3, name: "Gita Pun", imgurl: 66, url: ""}
-]
-app.get('/', function(req, res){
-   res.json(blog);
+  {
+    id: 1,
+    name: "Background Mask And Background Pattern For Divi",
+    imgurl:
+      "https://www.huppen.com.np/wp-content/uploads/2022/03/Blog-Cover-–-9.png",
+    url: "https://www.huppen.com.np/background-mask-and-background-pattern-for-divi/",
+  },
+  {
+    id: 2,
+    name: "New Teaching License Syllabus For Secondary And Lower Secondary Levels: TSC",
+    imgurl:
+      "https://www.huppen.com.np/wp-content/uploads/2022/03/Blog-Cover-–-6.png",
+    url: "https://www.huppen.com.np/new-teaching-license-syllabus-for-secondary-and-lower-secondary-levels-tsc/",
+  },
+  {
+    id: 3,
+    name: "A Quick Guide to ListView In Flutter - Hupen Design",
+    imgurl:
+      "https://www.huppen.com.np/wp-content/uploads/2021/12/Blog-Cover-–-4.png",
+    url: "https://www.huppen.com.np/a-quick-guide-to-listview-in-flutter-hupen-design/",
+  },
+  {
+    id: 4,
+    name: "Five useful flutter commands - Flutter development",
+    imgurl:
+      "https://www.huppen.com.np/wp-content/uploads/2021/08/Blog-Cover-–-3.png",
+    url: "https://www.huppen.com.np/five-useful-flutter-commands-flutter-development/",
+  },
+  {
+    id: 5,
+    name: "Five Websites To Find Freelance Jobs in 2021 For Web Developers",
+    imgurl:
+      "https://www.huppen.com.np/wp-content/uploads/2021/06/Blog-Cover-–-1.png",
+    url: "https://www.huppen.com.np/five-websites-to-find-freelance-jobs-in-2021-for-web-developers/",
+  },
+  {
+    id: 6,
+    name: "Eight Essential Website Design Steps Behind The Scene",
+    imgurl:
+      "https://www.huppen.com.np/wp-content/uploads/2021/04/Blog-Cover.png",
+    url: "https://www.huppen.com.np/eight-essential-website-design-steps-behind-the-scene/",
+  },
+  {
+    id: 7,
+    name: "Mahabir Pun A Visionary Social Entrepreneur",
+    imgurl: "https://www.huppen.com.np/wp-content/uploads/2021/04/Mahabir.jpg",
+    url: "https://www.huppen.com.np/mahabir-pun-a-visionary-social-entrepreneur/",
+  },
+];
+app.get("/", function (req, res) {
+  res.json(blog);
 });
 
 app.listen(process.env.PORT || 3000);
